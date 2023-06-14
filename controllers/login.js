4e0cb33edbc720db67ed9e97e4fb152d1149bcc1
Smmook <diegobix97@gmail.com>
2023-06-14 18:29:49 +0200

    increased token exp time

--- controllers/login.js
+++ controllers/login.js
@@ -23,7 +23,7 @@ loginRouter.post('/', async (req, res, next) => {
     }
 
     const token = jwt.sign(userForToken, process.env.SECRET, {
-      expiresIn: 120,
+      expiresIn: '24h',
     })
 
     res.status(200).send({ token, username: user.username, name: user.name })
